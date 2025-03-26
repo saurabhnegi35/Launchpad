b5eb3f058402bcd0885f294c6f25f363a415671e
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2025-03-26 15:57:49 +0530

    Hero Section UI Done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar";
 
 const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
 // import
 
-export default function Home() {
-  console.log("Server/Client");
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pattern pink_container">
@@ -11,6 +17,7 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get noticed in Virtual Competitions
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
